5d11017568c119bac6423e75d094bc993e9a95fb
lekz112 <lekz112@gmail.com>
2015-11-01 20:41:54 +0300

    Revert "Added rest login module"

--- app/src/main/java/com/triangleleft/flashcards/service/error/NetworkError.java
+++
@@ -1,9 +0,0 @@
-package com.triangleleft.flashcards.service.error;
-
-import android.support.annotation.Nullable;
-
-public class NetworkError extends CommonError {
-    public NetworkError(@Nullable String message) {
-        super(message);
-    }
-}
--- app/src/main/java/com/triangleleft/flashcards/service/stub/StubFlashcardsService.java
+++
@@ -1,7 +0,0 @@
-package com.triangleleft.flashcards.service.stub;
-
-import com.triangleleft.flashcards.service.IFlashcardsService;
-
-public class StubFlashcardsService implements IFlashcardsService {
-
-}
