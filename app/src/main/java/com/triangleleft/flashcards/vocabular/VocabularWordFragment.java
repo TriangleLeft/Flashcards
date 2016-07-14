eb3e4552a9b07979ed41fae08bf3974c2f6f4207
Aleksey Kurnosenko <lekz112@gmail.com>
2016-07-14 15:32:11 +0300

    Added voice-over for vocabulary words.

--- app/src/main/java/com/triangleleft/flashcards/vocabular/VocabularWordFragment.java
+++ app/src/main/java/com/triangleleft/flashcards/vocabular/VocabularWordFragment.java
@@ -1,5 +1,7 @@
 package com.triangleleft.flashcards.vocabular;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
@@ -10,7 +12,6 @@ import android.widget.TextView;
 import com.triangleleft.flashcards.R;
 import com.triangleleft.flashcards.common.BaseFragment;
 import com.triangleleft.flashcards.main.MainActivity;
-import com.triangleleft.flashcards.main.di.MainPageComponent;
 import com.triangleleft.flashcards.mvp.vocabular.IVocabularWordView;
 import com.triangleleft.flashcards.mvp.vocabular.VocabularWordPresenter;
 import com.triangleleft.flashcards.service.vocabular.VocabularyWord;
@@ -20,6 +21,8 @@ import com.triangleleft.flashcards.vocabular.di.VocabularWordComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -28,9 +31,8 @@ public class VocabularWordFragment
         extends BaseFragment<VocabularWordComponent, IVocabularWordView, VocabularWordPresenter>
         implements IVocabularWordView {
 
-    private static final Logger logger = LoggerFactory.getLogger(VocabularWordFragment.class);
     public static final String TAG = VocabularWordFragment.class.getSimpleName();
-
+    private static final Logger logger = LoggerFactory.getLogger(VocabularWordFragment.class);
     @Bind(R.id.vocabular_word_title)
     TextView titleView;
     @Bind(R.id.vocabular_word_translation_value)
@@ -45,6 +47,8 @@ public class VocabularWordFragment
     View genderEntry;
     @Bind(R.id.vocabular_word_pos_entry)
     View posEntry;
+    private VoiceOverPlayer voiceOverPlayer = new VoiceOverPlayer();
+    private VocabularyWord word;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -68,11 +72,21 @@ public class VocabularWordFragment
     @Override
     protected VocabularWordComponent buildComponent() {
         logger.debug("buildComponent() called");
-        return DaggerVocabularWordComponent.builder().mainPageComponent(getMainPageComponent()).build();
+        return DaggerVocabularWordComponent.builder()
+                .mainPageComponent(((MainActivity) getActivity()).getComponent())
+                .build();
     }
 
-    private MainPageComponent getMainPageComponent() {
-        return ((MainActivity) getActivity()).getComponent();
+    @Override
+    public void onPause() {
+        super.onPause();
+        voiceOverPlayer.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        voiceOverPlayer.onResume();
     }
 
     @NonNull
@@ -84,6 +98,7 @@ public class VocabularWordFragment
 
     @Override
     public void showWord(VocabularyWord word) {
+        this.word = word;
         titleView.setText(word.getWord());
         String translation = word.getTranslations().size() > 0 ? word.getTranslations().get(0) : "";
         translationView.setText(translation);
@@ -104,6 +119,39 @@ public class VocabularWordFragment
 
     @OnClick(R.id.vocabular_word_voice)
     public void onVoiceClick() {
+        voiceOverPlayer.voice(word);
+    }
 
+    private static class VoiceOverPlayer {
+        private final static String URL = "https://d7mj4aqfscim2.cloudfront.net/tts/%s/token/%s";
+        private MediaPlayer mediaPlayer;
+        private VocabularyWord currentWord;
+        private boolean hasCompleted;
+
+        public void onPause() {
+            mediaPlayer.release();
+        }
+
+        public void onResume() {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mediaPlayer.setOnPreparedListener(MediaPlayer::start);
+            mediaPlayer.setOnCompletionListener(mp -> hasCompleted = true);
+        }
+
+        public void voice(VocabularyWord word) {
+            // Start playing only if we want to voice some other word, or if we have completed playing
+            if (!word.equals(currentWord) || hasCompleted) {
+                hasCompleted = false;
+                try {
+                    mediaPlayer.reset();
+                    mediaPlayer.setDataSource(String.format(URL, word.getLearningLanguage(), word.getNormalizedWord()));
+                } catch (IOException e) {
+                    // Do nothing
+                }
+                mediaPlayer.prepareAsync();
+                currentWord = word;
+            }
+        }
     }
 }
