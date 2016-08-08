ed084e1499dff961878278241338b82e7929512a
Aleksey Kurnosenko <lekz112@gmail.com>
2016-08-08 17:14:48 +0300

    commit fix

--- ios/Flashcards/UI/Login/LoginViewController.swift
+++ ios/Flashcards/UI/Login/LoginViewController.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2016 TriangleLeft. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordView: UITextField!
+    @IBOutlet weak var rememberView: UISwitch!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func onRememberChanged(sender: UISwitch) {
+
+    }
+    
+    @IBAction func onLoginClick(sender: AnyObject) {
+        loginField.text = "login"
+        passwordView.text = "password"
+        rememberView.setOn(!rememberView.on, animated: true)
+    }
+    
+    
+}
