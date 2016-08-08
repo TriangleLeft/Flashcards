c2a1e619380e98c0b4cca81c12c14ccf7d2a1e26
Aleksey Kurnosenko <lekz112@gmail.com>
2016-08-08 17:11:39 +0300

    added simple ios project with login screen

---
+++ ios/Flashcards/FlashcardsUITests/FlashcardsUITests.swift
@@ -0,0 +1,36 @@
+//
+//  FlashcardsUITests.swift
+//  FlashcardsUITests
+//
+//  Created by Aleksey Kurnosenko on 08.08.16.
+//  Copyright © 2016 TriangleLeft. All rights reserved.
+//
+
+import XCTest
+
+class FlashcardsUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
---
+++ ios/Flashcards/UI/Login/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  Flashcards
+//
+//  Created by Aleksey Kurnosenko on 08.08.16.
+//  Copyright © 2016 TriangleLeft. All rights reserved.
+//
+
+import Foundation
