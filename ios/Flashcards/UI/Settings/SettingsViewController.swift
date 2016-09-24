651a88360b9df2e7ba2a6da739e17e0176851848
Aleksey Kurnosenko <lekz112@gmail.com>
2016-09-24 17:33:50 +0300

    iOS: add logout button to settings screen. Fixed login button not being hidden during login

--- ios/Flashcards/UI/Settings/SettingsViewController.swift
+++ ios/Flashcards/UI/Settings/SettingsViewController.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var tableViewCell: UITableViewCell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController?.navigationBar.translucent = false
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Done", style: .Done, target: self, action: #selector(SettingsViewController.onDoneClick))
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
     func onDoneClick() {
         dismissViewControllerAnimated(true, completion: nil)
     }
+}
 
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableViewCell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        AppDelegate.sharedAppDelegate().navigateToLogin()
+    }
 }
