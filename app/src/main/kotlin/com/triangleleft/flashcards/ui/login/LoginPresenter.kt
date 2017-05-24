1c161bedcbc5bf3a4d0b104169f3e8087d41e617
Aleksey Kurnosenko <lekz112@gmail.com>
2017-05-24 18:53:43 +0300

    Clean-up login presenter

--- app/src/main/kotlin/com/triangleleft/flashcards/ui/login/LoginPresenter.kt
+++ app/src/main/kotlin/com/triangleleft/flashcards/ui/login/LoginPresenter.kt
@@ -7,7 +7,9 @@ import com.triangleleft.flashcards.service.login.exception.LoginException
 import com.triangleleft.flashcards.service.login.exception.PasswordException
 import com.triangleleft.flashcards.ui.common.ViewAction
 import com.triangleleft.flashcards.ui.common.presenter.AbstractRxPresenter
+import com.triangleleft.flashcards.util.TextUtils
 import io.reactivex.Observable
+import io.reactivex.Observable.just
 import io.reactivex.ObservableTransformer
 import io.reactivex.Scheduler
 import org.slf4j.LoggerFactory
@@ -43,38 +45,72 @@ constructor(private val accountModule: AccountModule, private val loginModule: L
         // as we don't want to be stuck on progress page
         val state = savedViewState?.copy(page = LoginViewState.Page.CONTENT) ?: initialState
 
-        // Transform login button click into login request
-        val loginTransformer = ObservableTransformer<LoginClickEvent, ViewAction<LoginViewState>> { upstream ->
-            upstream.flatMap { (login, password) ->
-                loginModule.login(login, password)
-                        .map { _ -> LoginViewActions.success() }
-                        .startWith(LoginViewActions.progress())
-                        .onErrorReturn {
-                            when (it) {
-                                is LoginException -> LoginViewActions.loginError()
-                                is PasswordException -> LoginViewActions.passwordError()
-                                else -> LoginViewActions.genericError()
-                            }
-                        }
-            }
-        }
-
         // Map each event to appropriate view action
         val transformer = ObservableTransformer<LoginViewEvent, ViewAction<LoginViewState>> {
             events ->
-            events.publish { shared ->
-                Observable.merge(
-                        shared.ofType(SetLoginEvent::class.java).map { LoginViewActions.setLogin(it.login) },
-                        shared.ofType(SetPasswordEvent::class.java).map { LoginViewActions.setPassword(it.password) },
-                        shared.ofType(SetRememberUserEvent::class.java)
-                                .doOnNext { accountModule.setRememberUser(it.rememberUser) }
-                                .map { LoginViewActions.setRememberUser(it.rememberUser) },
-                        shared.ofType(LoginClickEvent::class.java).compose(loginTransformer)
-                )
+            events.flatMap {
+                when (it) {
+                    is SetLoginEvent -> just(LoginViewActions.setLogin(it.login))
+                    is SetPasswordEvent -> just(LoginViewActions.setPassword(it.password))
+                    is SetRememberUserEvent -> {
+                        accountModule.setRememberUser(it.rememberUser)
+                        just(LoginViewActions.setRememberUser(it.rememberUser))
+                    }
+                    is GenericErrorShownEvent -> Observable.empty()
+                    is LoginClickEvent -> loginModule.login(it.login, it.password)
+                            .map { _ -> LoginViewActions.success() }
+                            .startWith(LoginViewActions.progress())
+                            .onErrorReturn {
+                                when (it) {
+                                    is LoginException -> LoginViewActions.loginError()
+                                    is PasswordException -> LoginViewActions.passwordError()
+                                    else -> LoginViewActions.genericError()
+                                }
+                            }
+                }
             }
         }
+
         setup(viewEvents().compose(transformer), state)
     }
 
+    private object LoginViewActions {
+
+        fun setLogin(login: String): ViewAction<LoginViewState> {
+            return ViewAction { it.copy(login = login, loginButtonEnabled = isLoginButtonEnabled(login, it.password)) }
+        }
+
+        fun setPassword(password: String): ViewAction<LoginViewState> {
+            return ViewAction { it.copy(password = password, loginButtonEnabled = isLoginButtonEnabled(it.login, password)) }
+        }
+
+        private fun isLoginButtonEnabled(login: String, password: String): Boolean {
+            return TextUtils.hasText(login) && TextUtils.hasText(password)
+        }
+
+        fun setRememberUser(rememberUser: Boolean): ViewAction<LoginViewState> {
+            return ViewAction { it.copy(shouldRememberUser = rememberUser) }
+        }
+
+        fun loginError(): ViewAction<LoginViewState> {
+            return ViewAction { it.copy(page = LoginViewState.Page.CONTENT, hasLoginError = true) }
+        }
+
+        fun passwordError(): ViewAction<LoginViewState> {
+            return ViewAction { it.copy(page = LoginViewState.Page.CONTENT, hasPasswordError = true) }
+        }
+
+        fun genericError(): ViewAction<LoginViewState> {
+            return ViewAction { it.copy(page = LoginViewState.Page.CONTENT, hasGenericError = true) }
+        }
+
+        fun success(): ViewAction<LoginViewState> {
+            return ViewAction { it.copy(page = LoginViewState.Page.SUCCESS) }
+        }
+
+        fun progress(): ViewAction<LoginViewState> {
+            return ViewAction { it.copy(page = LoginViewState.Page.PROGRESS) }
+        }
+    }
 
 }
